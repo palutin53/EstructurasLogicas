71390f1dc0d67031abd97e22a58e4741f794e4bd
palutin53 <palutin31@gmail.com>
2016-07-30 09:48:29 -0600

    Pseudo codigo tarea

---
+++ Semana1/PseudoCodigoTarea/PseudoCodigoTarea/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PseudoCodigoTarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arreglos");
+            int[] LA = { 1, 3, 5, 7, 8 };
+            int k = 3; int n = 5;
+            int i, j;
+
+            Console.WriteLine("" + LA[0] + ", " + LA[1] + ", " + LA[2] + ", " + LA[3] + ", " + LA[4]);
+            Console.WriteLine("K=" + k + " n=" + n);
+            Console.WriteLine("int=i,j");
+            Console.ReadKey();
+
+            j = k;
+            while (j < n)
+            {
+                LA[j - 1] = LA[j];
+                j = j + 1;
+
+            }
+            n = n - 1;
+
+            Console.WriteLine("Desarrollo del arreglo");
+            Console.WriteLine("" + LA[0] + ", " + LA[1] + ", " + LA[2] + ", " + LA[3] + ", " + LA[4]);
+            Console.ReadKey();
+        }
+    }
+}
