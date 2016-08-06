d8bc5aa2af8fa1eaf130d0090a44f0f1b8769dfb
palutin53 <palutin31@gmail.com>
2016-08-06 10:01:33 -0600

    opcion de salida actualizada

--- Semana 2/Ejercicio/Ejercicio/Program.cs
+++ Semana 2/Ejercicio/Ejercicio/Program.cs
@@ -43,48 +43,146 @@ namespace Ejercicio
 
                         //Ejercicio 1
 
-                        Console.WriteLine("Ingresa 3 letras y le cambiaremos el orden.");
-                        Console.WriteLine("Ingresa la primera letra:");
-                        string letra1 = Console.ReadLine();
-                        Console.WriteLine("Ingresa la segunda letra:");
-                        string letra2 = Console.ReadLine();
-                        Console.WriteLine("Ingresa la tercera letra:");
-                        string letra3 = Console.ReadLine();
-                        Console.WriteLine("Presiona enter para ver el orden inverso.");
-                        while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                        Console.WriteLine("La cadena quedan así: " + letra3 + "  " + letra2 + "  " + letra1);
-                        Console.WriteLine("Presiona enter para volver al menú");
-                        while(Console.ReadKey().Key != ConsoleKey.Enter) { }
-                        Console.Clear();
-                        valid = true;
+                        bool valid1 = true;
+                        while (valid1)
+                        {
+
+                            Console.WriteLine("Ingresa 3 letras y le cambiaremos el orden.");
+                            Console.WriteLine("Ingresa la primera letra:");
+                            string letra1 = Console.ReadLine();
+                            Console.WriteLine("Ingresa la segunda letra:");
+                            string letra2 = Console.ReadLine();
+                            Console.WriteLine("Ingresa la tercera letra:");
+                            string letra3 = Console.ReadLine();
+                            Console.WriteLine("Presiona enter para ver el orden inverso.");
+                            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                            Console.WriteLine("La cadena quedan así: " + letra3 + "  " + letra2 + "  " + letra1);
+                            Console.WriteLine("Presiona enter para volver al menú");
+                            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                            Console.Clear();
+                            valid = true;
+
+
+                            Console.WriteLine("¿Quieres intentar de nuevo?\n 1. Si\n 2. No");
+                            int salir1 = int.Parse(Console.ReadLine());
+
+                            switch (salir1)
+                            {
+                                case 1:
+                                    Console.WriteLine("presiona enter para retornar.");
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                    valid1 = true;
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Presiona enter para salir.");
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                    valid1 = false;
+                                    break;
+                                default:
+                                    Console.WriteLine("Selección invalida.");
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                    valid1 = true;
+                                    break;
+                            }
+                        }
+
                         break;
                     case 2:
 
                         // Ejercicio 2
 
-                        int num, lineas, ancho;
-                        Console.WriteLine("Introduzca un numero de 1 a 9 para crear un triangulo.");
-                        num = int.Parse(Console.ReadLine());
-                        Console.Clear();
-                        for (lineas = 1; lineas <= num; lineas++)
+                        bool valid2 = true;
+                        while (valid2)
                         {
-                            for (ancho = 0; ancho <= num-lineas; ancho++)
+
+                            int num, lineas, ancho;
+                            Console.WriteLine("Introduzca un numero de 1 a 9 para crear un triangulo.");
+                            num = int.Parse(Console.ReadLine());
+                            Console.Clear();
+                            for (lineas = 1; lineas <= num; lineas++)
                             {
-                                Console.Write(num);
+                                for (ancho = 0; ancho <= num - lineas; ancho++)
+                                {
+                                    Console.Write(num);
+                                }
+                                Console.WriteLine();
                             }
-                            Console.WriteLine();
+                            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                            Console.Clear();
+
+                            Console.WriteLine("¿Quieres intentar de nuevo?\n 1. Si\n 2. No");
+                            int salir2 = int.Parse(Console.ReadLine());
+
+                            switch (salir2)
+                            {
+                                case 1:
+                                    Console.WriteLine("presiona enter para retornar.");
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                    valid2 = true;
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Presiona enter para salir.");
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                    valid2 = false;
+                                    break;
+                                default:
+                                    Console.WriteLine("Selección invalida.");
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                    valid2 = true;
+                                    break;
+                            }
                         }
-                        while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                        Console.Clear();
+
                         break;
                     case 3:
 
-                        for (int login = 1; login <= 3; login++)
+                        bool valid3 = true;
+                        while (valid3)
+                        {
+                            int login = 1;
+                        for (login = 1; login <= 3; login++)
                         {
                             Console.WriteLine("¡Bienvenido! Ingresa tu contraseña: ");
                             Console.ReadLine();
+                            Console.WriteLine("Contraseña incorrecta.");
+                            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
                             Console.Clear();
                         }
+                        Console.WriteLine("¿Quieres intentar de nuevo?\n 1. Si\n 2. No");
+                        int salir3 = int.Parse(Console.ReadLine());
+                            
+                            switch (salir3)
+                            {
+                                case 1:
+                                    Console.WriteLine("presiona enter para retornar.");
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                    valid3 = true;
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Presiona enter para salir.");
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                    valid3 = false;
+                                    break;
+                                default:
+                                    Console.WriteLine("Selección invalida.");
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                    valid3 = true;
+                                    break;
+                            }
+                        }
+                        break;
+                    case 4:
+
+
 
                         break;
                     default:
