d12f62caeba9a7124b6c3a0f9eaba4da3e65d02c
palutin53 <palutin31@gmail.com>
2016-08-08 21:19:41 -0600

    update ejercicio 4

--- Semana 2/Ejercicio/Ejercicio/Program.cs
+++ Semana 2/Ejercicio/Ejercicio/Program.cs
@@ -10,6 +10,7 @@ namespace Ejercicio
     {
         static void Main(string[] args)
         {
+
             //inicio
 
             Console.WriteLine("¡Hola! ingresa tu nombre: ");
@@ -23,6 +24,7 @@ namespace Ejercicio
             bool valid = true;
             while (valid)
             {
+                
                 //Menú
 
                 int opcion = 0;
@@ -34,17 +36,19 @@ namespace Ejercicio
                 Console.WriteLine(" 5. Ejercicio 5");
                 Console.WriteLine(" 6. Ejercicio 6");
                 opcion = int.Parse(Console.ReadLine());
-                Console.Clear(); 
+                Console.Clear();
+
+                //validacion para sub-menu
+                bool validsub = true;
 
                 switch (opcion)
                 {
-
+                    
                     case 1:
 
                         //Ejercicio 1
 
-                        bool valid1 = true;
-                        while (valid1)
+                        while (validsub)
                         {
 
                             Console.WriteLine("Ingresa 3 letras y le cambiaremos el orden.");
@@ -57,36 +61,9 @@ namespace Ejercicio
                             Console.WriteLine("Presiona enter para ver el orden inverso.");
                             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
                             Console.WriteLine("La cadena quedan así: " + letra3 + "  " + letra2 + "  " + letra1);
-                            Console.WriteLine("Presiona enter para volver al menú");
-                            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                            Console.Clear();
-                            valid = true;
 
+                           
 
-                            Console.WriteLine("¿Quieres intentar de nuevo?\n 1. Si\n 2. No");
-                            int salir1 = int.Parse(Console.ReadLine());
-
-                            switch (salir1)
-                            {
-                                case 1:
-                                    Console.WriteLine("presiona enter para retornar.");
-                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                    Console.Clear();
-                                    valid1 = true;
-                                    break;
-                                case 2:
-                                    Console.WriteLine("Presiona enter para salir.");
-                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                    Console.Clear();
-                                    valid1 = false;
-                                    break;
-                                default:
-                                    Console.WriteLine("Selección invalida.");
-                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                    Console.Clear();
-                                    valid1 = true;
-                                    break;
-                            }
                         }
 
                         break;
@@ -94,8 +71,7 @@ namespace Ejercicio
 
                         // Ejercicio 2
 
-                        bool valid2 = true;
-                        while (valid2)
+                        while (validsub)
                         {
 
                             int num, lineas, ancho;
@@ -113,37 +89,12 @@ namespace Ejercicio
                             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
                             Console.Clear();
 
-                            Console.WriteLine("¿Quieres intentar de nuevo?\n 1. Si\n 2. No");
-                            int salir2 = int.Parse(Console.ReadLine());
-
-                            switch (salir2)
-                            {
-                                case 1:
-                                    Console.WriteLine("presiona enter para retornar.");
-                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                    Console.Clear();
-                                    valid2 = true;
-                                    break;
-                                case 2:
-                                    Console.WriteLine("Presiona enter para salir.");
-                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                    Console.Clear();
-                                    valid2 = false;
-                                    break;
-                                default:
-                                    Console.WriteLine("Selección invalida.");
-                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                    Console.Clear();
-                                    valid2 = true;
-                                    break;
-                            }
                         }
 
                         break;
                     case 3:
 
-                        bool valid3 = true;
-                        while (valid3)
+                        while (validsub)
                         {
                             int login = 1;
                         for (login = 1; login <= 3; login++)
@@ -154,72 +105,111 @@ namespace Ejercicio
                             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
                             Console.Clear();
                         }
-                        Console.WriteLine("¿Quieres intentar de nuevo?\n 1. Si\n 2. No");
-                        int salir3 = int.Parse(Console.ReadLine());
-                            
-                            switch (salir3)
-                            {
-                                case 1:
-                                    Console.WriteLine("presiona enter para retornar.");
-                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                    Console.Clear();
-                                    valid3 = true;
-                                    break;
-                                case 2:
-                                    Console.WriteLine("Presiona enter para salir.");
-                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                    Console.Clear();
-                                    valid3 = false;
-                                    break;
-                                default:
-                                    Console.WriteLine("Selección invalida.");
-                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                    Console.Clear();
-                                    valid3 = true;
-                                    break;
-                            }
+                        
                         }
                         break;
                     case 4:
 
-                        bool valid4 = true;
-                        while ()
+                        while (validsub)
                         {
 
                             Console.WriteLine("Ok, vamos a hacer operaciones aritmeticas, ingresa los numeros y la operacion (+ , - , * , /) que desees realizar.");
+                            Console.WriteLine("Presiona enter cuando estés listo.");
+                            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                            Console.Clear();
+                            Console.WriteLine("Recuerda que puedes hacer 4 básicas operaciones (+ , - , * , / ).");
+                            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                            Console.WriteLine("Ok, ahora ingresa el primer numero para operar.");
+                            double num1 = int.Parse(Console.ReadLine());
+                            Console.WriteLine("¿Qué operación quieres realizar?.");
+                            char op = char.Parse(Console.ReadLine());
+                            Console.WriteLine("Y el segundo numero.");
+                            double num2 = int.Parse(Console.ReadLine());
+
+                            if (op == '+' || op == '-' || op == '*' || op == '/')
+                            {
+                                if(op == '+')
+                                {
+                                    double suma = num1 + num2;
+                                    Console.WriteLine("El resultado de "+num1+" "+op+" "+num2+" es: "+suma);
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                }
+                                else { }
+                                if (op == '-')
+                                {
+                                    double resta = num1 - num2;
+                                    Console.WriteLine("El resultado de " + num1 + " " + op + " " + num2 + " es: " + resta);
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                }
+                                else { }
+                                if (op == '*')
+                                {
+                                    double multi = num1 * num2;
+                                    Console.WriteLine("El resultado de " + num1 + " " + op + " " + num2 + " es: " + multi);
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                }
+                                else { }
+                                if (op == '/')
+                                {
+                                    double div = num1 / num2;
+                                    Console.WriteLine("El resultado de " + num1 + " " + op + " " + num2 + " es: " + div);
+                                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                    Console.Clear();
+                                }
+                                else { }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Operador incorrecto.");
+                                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                                Console.Clear();
+                                validsub = true;
+                            }
 
                         }
 
-                        Console.WriteLine("¿Quieres intentar de nuevo?\n 1. Si\n 2. No");
-                        int salir3 = int.Parse(Console.ReadLine());
-
-                        switch (salir3)
-                        {
-                            case 1:
-                                Console.WriteLine("presiona enter para retornar.");
-                                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                Console.Clear();
-                                valid3 = true;
-                                break;
-                            case 2:
-                                Console.WriteLine("Presiona enter para salir.");
-                                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                Console.Clear();
-                                valid3 = false;
-                                break;
-                            default:
-                                Console.WriteLine("Selección invalida.");
-                                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                                Console.Clear();
-                                valid3 = true;
-                                break;
-                        }
-
+                        break;
+                    case 5:
                         break;
                     default:
                         break;
                 }
             }
         }
+
+        //Funcion salir 
+
+        static void salir()
+        {
+            Console.WriteLine("¿Quieres intentar de nuevo?\n 1. Si\n 2. No");
+            int salir = int.Parse(Console.ReadLine());
+            bool validsub = true;
+
+            switch (salir)
+            {
+                case 1:
+                    Console.WriteLine("presiona enter para retornar.");
+                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                    Console.Clear();
+                    validsub = true;
+                    break;
+                case 2:
+                    Console.WriteLine("Presiona enter para salir.");
+                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                    Console.Clear();
+                    validsub = false;
+                    break;
+                default:
+                    Console.WriteLine("Selección invalida.");
+                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                    Console.Clear();
+                    validsub = true;
+                    break;
+            }
+        }
+
     }
 }
