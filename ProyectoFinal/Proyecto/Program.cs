ebf566a423ed7780e1b82afaad4df62a8bfcc7ae
palutin53 <palutin31@gmail.com>
2016-08-26 18:26:55 -0600

    update 26082016
    18:26

--- ProyectoFinal/Proyecto/Program.cs
+++ ProyectoFinal/Proyecto/Program.cs
@@ -23,7 +23,9 @@ namespace Proyecto
                 int contador = 0, sum = 0;
                 char signo;
 
-                Console.WriteLine("Selecciona según lo que quieras hacer:\n 1. Instrucciones\n 2. Calculadora\n 3. SALIR");
+                string menu = "Selecciona según lo que quieras hacer:";
+                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (menu.Length / 2)) + "}", menu));
+                Console.WriteLine("\n 1. Instrucciones\n 2. Calculadora\n 3. Desarrollador \n 4. SALIR");
                 int opcion = int.Parse(Console.ReadLine());
                 Console.Clear();
 
@@ -32,7 +34,19 @@ namespace Proyecto
                     case 1:
                         while (validsub)
                         {
-                            Console.WriteLine("");
+                            string titulo = "PROYECTO FINAL";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (titulo.Length / 2)) + "}", titulo));
+                            enter();
+                            string linea1 = "Esta es una calculadora capaz de reconocer singos positivos, negativos,";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (linea1.Length / 2)) + "}", linea1));
+                            string linea2 = "operaciones aritmeticas puedes relizar varias operaciones aritmeticas";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (linea2.Length / 2)) + "}", linea2));
+                            string linea3  = "y obtener los resultados exactos, tienes las operaciones de suma, resta,";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (linea3.Length / 2)) + "}", linea3));
+                            string linea4 = "multiplicacion y division. Tienes que presionar enter despues de cada caracter";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (linea4.Length / 2)) + "}", linea4));
+                            string linea5 = "que ingresas ya sea numero, operadores o signos de agrupación.";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (linea5.Length / 2)) + "}", linea5));
                             enter();
                             Console.Clear();
                             validsub = salir(arreglo);
@@ -68,7 +82,6 @@ namespace Proyecto
                                     {
 
                                         Console.Write(s);
-
                                     }
                                     Console.Clear();
                                     Console.WriteLine("Ingresa un numero");
@@ -122,22 +135,25 @@ namespace Proyecto
                                     Console.Clear();
                                     if (signo == '=')
                                     {
-                                        Console.WriteLine("La respuesta de");
+                                        string resp = "La respuesta de";
+                                        Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (resp.Length / 2)) + "}", resp));
                                         foreach (string s in arreglo)
                                         {
                                             Console.Write(s);
                                         }
-                                        Console.WriteLine("\nes: " + a);
+                                        string es = "\nes: " + a;
+                                        Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (es.Length / 2)) + "}", es));
+
                                         enter();
                                         Console.Clear();
                                         validsub = salir(arreglo);
                                     }
                                     else
                                     {
-                                        Console.WriteLine("Ingres un numero");
+                                        Console.Write("Ingres un numero");
                                         foreach (string s in arreglo)
                                         {
-                                            Console.Write(s);
+                                            Console.WriteLine(s);
                                         }
                                         y = int.Parse(Console.ReadLine());
                                         arreglo[sum] = Convert.ToString(y);
@@ -176,24 +192,48 @@ namespace Proyecto
                         //----------------------------------------------------------------------------------------------------------------
                         break;
                     case 3:
-                        Console.WriteLine("¿Quieres salir?\n 1. Si\n 2. No");
+                        while (validsub)
+                        {
+                            string minombre = "Pablo Andrés Lutin Gomez";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (minombre.Length / 2)) + "}", minombre));
+                            enter();
+                            string carrera = "Ingeniería en Sistemas";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (carrera.Length / 2)) + "}", carrera));
+                            enter();
+                            string carne = "Carné: 160402009";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (carne.Length / 2)) + "}", carne));
+                            enter();
+                            string proyecto = "Proyecto Final Estructuras Logicas.";
+                            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (proyecto.Length / 2)) + "}", proyecto));
+                            enter();
+                            Console.Clear();
+                            validsub = salir(arreglo);
+                        }
+                        break;
+                    case 4:
+                        string qui = "¿Quieres salir?";
+                        Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (qui.Length / 2)) + "}", qui));
+                        Console.WriteLine("\n 1. Si\n 2. No");
                         int salida = int.Parse(Console.ReadLine());
                         switch (salida)
                         {
                             case 1:
-                                Console.WriteLine("Presiona enter para salir.");
+                                string pres = "Presiona enter para salir.";
+                                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (pres.Length / 2)) + "}", pres));
                                 enter();
                                 valid = false;
                                 Console.Clear();
                                 break;
                             case 2:
-                                Console.WriteLine("Presiona enter para retornar.");
+                                string prer = "Presiona enter para retornar.";
+                                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (prer.Length / 2)) + "}", prer));
                                 enter();
                                 valid = true;
                                 Console.Clear();
                                 break;
                             default:
-                                Console.WriteLine("Valor invalido, Presiona enter.");
+                                string valinv = "Valor invalido, Presiona enter.";
+                                Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (valinv.Length / 2)) + "}", valinv));
                                 enter();
                                 valid = true;
                                 Console.Clear();
@@ -214,12 +254,15 @@ namespace Proyecto
         {
             int borrar = 0;
             bool validsub = true;
-            Console.WriteLine("¿Quieres intentar de nuevo?\n 1. Si\n 2. No");
+            string sal = "¿Quieres intentar de nuevo?";
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (sal.Length / 2)) + "}", sal));
+            Console.WriteLine("\n 1. Si\n 2. No");
             int salida = int.Parse(Console.ReadLine());
             switch (salida)
             {
                 case 1:
-                    Console.WriteLine("Presiona enter para retornar.");
+                    string retorno = "Presiona enter para retornar.";
+                    Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (retorno.Length / 2)) + "}", retorno));
                     enter();
                     validsub = true;
                     for (borrar = 0; borrar < 100; borrar++)
@@ -229,7 +272,8 @@ namespace Proyecto
                     Console.Clear();
                     break;
                 case 2:
-                    Console.WriteLine("Presiona enter para salir.");
+                    string sali = "Presiona enter para salir.";
+                    Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (sali.Length / 2)) + "}", sali));
                     enter();
                     validsub = false;
                     for (borrar = 0; borrar < 100; borrar++)
@@ -239,7 +283,8 @@ namespace Proyecto
                     Console.Clear();
                     break;
                 default:
-                    Console.WriteLine("Valor invalido, Presiona enter.");
+                    string inval = "Valor invalido, Presiona enter.";
+                    Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (inval.Length / 2)) + "}", inval));
                     enter();
                     validsub = false;
                     for (borrar = 0; borrar < 100; borrar++)
